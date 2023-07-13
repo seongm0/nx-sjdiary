a68e38903a547a4f26e9786867329a5d3b39a91d
seongmo <eodah4115@gmail.com>
2023-07-13 21:27:20 +0900

    feat(front): init

--- apps/front/webpack.config.js
+++ apps/front/webpack.config.js
@@ -3,7 +3,7 @@ const { withReact } = require('@nx/react');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
-  // Update the webpack config as needed here.
-  // e.g. `config.plugins.push(new MyPlugin())`
+  // Update the webpack configs as needed here.
+  // e.g. `configs.plugins.push(new MyPlugin())`
   return config;
 });
